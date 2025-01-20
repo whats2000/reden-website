25fc6cfa2c2cb5bc148ad3edbe9061e01c51af2c
Liyan Zhao <return65535@qq.com>
2025-01-20 10:17:27 +0800

    fix

--- pages/litematica/review.vue
+++ pages/litematica/review.vue
@@ -7,14 +7,30 @@ import type {
   ListLitematicaResponse,
   MachineDef,
 } from '~/pages/litematica/index.vue';
+import { toast } from 'vuetify-sonner';
+
+definePageMeta({
+  title: 'Review Litematica',
+  needLogin: true,
+});
 
 const page = ref(1);
-const { data, status } = useFetch<ListLitematicaResponse>(
+const { data, status, error } = useFetch<ListLitematicaResponse>(
   () => `/api/mc-services/litematica/archiver-review?page=${page.value}`,
 );
 
+watch(error, (value) => {
+  if (value) {
+    toast.error(value.message);
+    if (value.statusCode === 403) {
+      throw value;
+    }
+  }
+});
+
 const localizedData = ref<Record<string, MachineDef>>();
 const currentLoadedData = ref<string>();
+const rejectReason = ref<string>();
 </script>
 
 <template>
@@ -90,6 +106,54 @@ const currentLoadedData = ref<string>();
               />
             </div>
           </v-card>
+        </v-dialog>
+      </v-btn>
+      <v-btn
+        color="success"
+        icon
+        @click="
+          async () => {
+            await doFetchPost(
+              `/api/mc-services/yisibite/${item.key}/approve`,
+              {},
+            );
+            toast.success('已通过');
+          }
+        "
+      >
+        <v-icon>mdi-check</v-icon>
+      </v-btn>
+      <v-btn color="error" icon>
+        <v-icon>mdi-close</v-icon>
+        <v-dialog activator="parent" close-on-back max-width="900">
+          <v-form
+            @submit.prevent="
+              async (e) => {
+                if (!(await e).valid) {
+                  return;
+                }
+                await doFetchPost(
+                  `/api/mc-services/yisibite/${item.key}/reject`,
+                  { reason: rejectReason },
+                );
+                toast.success('已拒绝');
+              }
+            "
+          >
+            <v-card variant="flat">
+              <v-card-text>
+                <v-text-field
+                  v-model="rejectReason"
+                  label="拒绝理由"
+                  outlined
+                />
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn color="error" type="submit">拒绝</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-form>
         </v-dialog>
       </v-btn>
     </template>
